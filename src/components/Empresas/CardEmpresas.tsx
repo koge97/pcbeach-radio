c03f64b9bfc65e34291e6f6b8eca678c0f6d4059
koge97 <manuel_yavar@outlook.com>
2023-10-17 00:27:59 -0300

    🎨 Fix icono en modo oscuro

--- src/components/Empresas/CardEmpresas.tsx
+++ src/components/Empresas/CardEmpresas.tsx
@@ -16,7 +16,7 @@ function CardEmpresas(Props: PropsEmpresas) {
     return (
         <div className='flex-initial w-72 md:w-96  p-4 dark:bg-zinc-800 border border-slate-200/20 dark:border-slate-800/20 shadow-xl rounded-xl'>
             <div className="hero-content w-full h-full justify-start flex flex-col">
-                <Props.icono className="w-8 h-8 text-white" />
+                <Props.icono className="w-8 h-8 text-slate-800 dark:text-white" />
                 <h2 className={`text-4xl font-bold ${Props.atributo}`}>{Props.titulo}</h2>
                 <p className="py-6">{Props.subtitulo}</p>
                 <a className="mt-auto" href={Props.href} target="_blank" rel="noreferrer">
